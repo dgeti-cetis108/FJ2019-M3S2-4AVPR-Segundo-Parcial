32aaf8f2f92c5bf249c5e2770b0b1169289faac1
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-04-11 09:23:35 -0600

    Carpeta del parcial
    Expressjs, sqlite3 installed
    express server configured
    db connection, ddl, dml of users table

---
+++ 04-Parcial/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const sqlite = require('sqlite3').verbose()
+
+// conectar a la base de datos
+const db = new sqlite.Database('./db/cetis108.db', (err) => {
+  if (err) {
+    console.log(err.message)
+  }
+  console.log('Conectado a la base de datos correctamente!')
+})
+
+// definir una consulta
+let sql = "select * from users"
+
+// ejecutando la consulta
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    console.log(err.message)
+  }
+  rows.forEach((row) => {
+    console.log(`Nombre: ${row.firstname} Apellidos: ${row.lastname}`)
+  })
+})
+
+
+// app.get('/', (req, res) => res.send('Hola Dummie!'))
+app.use(express.static('src'))
+
+app.listen(port, () => console.log(`Estoy escuchando en el puerto ${port}`))
